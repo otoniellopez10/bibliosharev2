134b82df85ffce953723f86b88e7cf6c234e22bd
Otoniel López <olopez@marnagarage.com>
2022-01-12 19:28:02 -0400

    cambios dashboard y mejora de menu

--- views/assets/js/admin-menu.js
+++ views/assets/js/admin-menu.js
@@ -1,5 +1,16 @@
 $(document).ready(function () {
     $("#sessionUserName").text(sessionStorage.user);
+
+    // var links = $("#menu-admin a");
+    // for(let i = 0; i <= links.length; i++){
+    //     console.log(links[i])
+    // }
+    $("#menu-admin a").each(function(indice,elemento){
+        let hash = $(location).attr('pathname');
+        let href = $(this).attr("href")
+        
+        if(href == hash) $(this).addClass("active");
+    })
 });
 
 $(".btn-logout").click(function (e) {
